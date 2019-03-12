b3d609da75d851c87c2faa429f940994b9300027
Sampath Kumar <sam1990kumar@gmail.com>
2019-03-12 19:25:38 +0530

    Added a python multi-processing code for executing multiple os commands.

--- vitaflow/annotate_server/bin/utils.py
+++ vitaflow/annotate_server/bin/utils.py
@@ -44,3 +44,40 @@ def get_folder_config(dir_path, file_exts, trim_path_prefix):
             'file': file
         }
     return file_dict
+
+
+def remove_extra_lines_from_string(inputstring):
+    string_to_return = ""
+    for line in inputstring.split("\n"):
+        if len(line.strip()) > 0:  # Only add non empty lines to the string_to_return
+            string_to_return = string_to_return + line
+    return string_to_return
+
+
+def runCmd(cmd):
+    # string of a command passed in here
+    from subprocess import run, PIPE
+    string_to_return = str(run(cmd, shell=True, stdout=PIPE).stdout.decode('utf-8'))
+    string_to_return = remove_extra_lines_from_string(string_to_return)
+    return string_to_return
+
+
+def run_parallel_cmds(list_of_commands):
+    from multiprocessing.dummy import Pool
+    # thread pool
+    from subprocess import Popen, PIPE, STDOUT
+    listofprocesses = [Popen(list_of_commands[i], shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+                       for i in range(len(list_of_commands))]
+
+    # Python calls this list comprehension, it's a way of making a list
+    def get_outputs(process):  # MultiProcess Thread Pooling require you to map to a function, thus defining a function.
+        return process.communicate()[0]  # process is object of type subprocess.Popen
+
+    outputs = Pool(len(list_of_commands)).map(get_outputs,
+                                              listofprocesses)  # outputs is a list of bytes (which is a type of string)
+    listofoutputstrings = []
+    for i in range(len(list_of_commands)):
+        outputasstring = remove_extra_lines_from_string(
+            outputs[i].decode('utf-8'))  # .decode('utf-8') converts bytes to string
+        listofoutputstrings.append(outputasstring)
+    return listofoutputstrings
